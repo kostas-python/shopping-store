f0d40f4727b07b030ac34bbc2bc0353ff616f48a
Kostas <diamantopoulos.kostas@hotmail.com>
2024-03-21 00:01:14 +0000

    initial commit

---
+++ src/app/layout/top-bar/top-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
+
+
+
+
+@Component({
+  selector: 'app-top-bar',
+  standalone: true,
+  imports: [DecimalPipe, RouterLink],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.css'
+})
+export class TopBarComponent {
+
+}
