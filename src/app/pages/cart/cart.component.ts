4f199f62dd35d08acbfc707050161e1d2b9df2c8
Kostas <diamantopoulos.kostas@hotmail.com>
2024-03-23 18:29:16 +0000

    general changes

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,12 +1,13 @@
 import { Component, computed } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import { CartItemCardComponent } from './components/cart-item-card/cart-item-card.component';
 
 
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CartItemCardComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
@@ -20,5 +21,21 @@ export class CartComponent {
   
    
   constructor(private cartService: CartService) {}
+
+  onItemQuantityUpdate(quantity: number, id: string) {
+    let increase = true;
+    const item = this.items().find((t) => t.id === id);
+    if (quantity < item!.quantity) increase = false;
+    if (increase) {
+      this.cartService.increaseItem(item!);
+    } else {
+      this.cartService.decreaseItem(item!);
+    }
+  }
+
+  onRemoveItem(id: string) {
+    const item = this.items().find((t) => t.id === id);
+    this.cartService.removeItem(item!);
+  }
   
 }
