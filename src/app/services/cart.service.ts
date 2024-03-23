466019adb33a2b2f0275e91210d17d6ecfab125d
Kostas <diamantopoulos.kostas@hotmail.com>
2024-03-23 12:36:33 +0000

    added item function in cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,17 +1,38 @@
 import { Injectable, signal } from '@angular/core';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class CartService {
-  cart = signal<Cart>({
+  cart = signal<Cart> ({
     items: [],
     count: 0,
     total: 0,
   });
   
   constructor() { }
+
+  addItem(item: CartItem) {
+    const itemObj = this.cart().items.find((t) => t.id ===item.id);
+    if (itemObj) {
+      // increase item
+
+    } else {
+      this.cart.update((prevCart) => ({
+    ...prevCart,
+    items: [...prevCart.items,item],
+    count: prevCart.count +1,
+    total: prevCart.total +item.price,
+    
+    }));
+  }
+ }
 }
+
 
 export interface CartItem {
   id: string;
@@ -20,6 +41,7 @@ export interface CartItem {
   price: number;
   quantity: number;
 }
+
 
 export interface Cart {
   items: CartItem[];
