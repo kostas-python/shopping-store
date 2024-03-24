dab9cfdbb1252a8891a48a2145d24bb8f02fde4b
Kostas <diamantopoulos.kostas@hotmail.com>
2024-03-24 13:09:33 +0000

    cancel comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { SuccessComponent } from './pages/success/success.component';
+import { CancelComponent } from './pages/cancel/cancel.component';
 
 
 
@@ -10,4 +11,5 @@ export const routes: Routes = [
     { path: 'home' , component: HomeComponent },
     { path: 'cart' , component: CartComponent },
     { path: 'success' , component: SuccessComponent },
+    { path: 'cancel' , component: CancelComponent },
 ];
